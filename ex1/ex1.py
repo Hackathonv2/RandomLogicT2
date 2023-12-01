834706f5a5b9273d9a93632563c66732efeba206
HARY Ethan <milo.bonbaril@gmail.com>
2023-12-01 09:19:09 +0100

    [ADD] added ex1

---
+++ ex1/ex1.py
@@ -0,0 +1,21 @@
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('number', type=int, help='Enter a number from 1 to 1000')
+args = parser.parse_args()
+
+if args.number < 1 or args.number > 1000:
+    print('Error: Number must be between 1 and 1000')
+    exit(1)
+
+n = args.number
+for i in range (4):
+    if n % 3 == 0:
+        print(f"divided {n} by 3")
+        n = n / 3
+    elif n % 2 == 0:
+        print(f"divided {n} by 2")
+        n = n / 2
+    else:
+        print(f"subtracted {n} by 1")
+        n = n - 1
